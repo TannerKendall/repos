e8ae83f2f78c25786ff9a75269fd57fccaa92929
Tanner Kendall <tannerkendall@gmail.com>
2021-01-11 23:35:19 -0500

    Hello World in NetBeans

---
+++ classwork/HelloWorld/src/main/java/com/tk/helloworld/Hello.java
@@ -0,0 +1,13 @@
+/*
+ *@author Tanner Kendall
+ *email: tannerkendall@gmail.com
+ *date: 1/11/21
+ *purpose: my first Hello World program in an IDE
+ */
+package com.tk.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tanner!");
+    }
+}
