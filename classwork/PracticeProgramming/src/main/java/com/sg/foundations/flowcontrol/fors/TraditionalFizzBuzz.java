858d301c480c98228ef97bc066cd5e153b5af976
Tanner Kendall <tannerkendall@gmail.com>
2021-01-22 15:08:39 -0500

    1/22/21 again

---
+++ classwork/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/fors/TraditionalFizzBuzz.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.flowcontrol.fors;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tanner Kendall
+ */
+public class TraditionalFizzBuzz {
+    public static void main(String[] args) {
+        
+        Scanner inputReader = new Scanner(System.in);
+        
+        System.out.println("How many units of fizzing and buzzing do you need in your life");
+        int units = inputReader.nextInt();
+        
+        for (int i = 0; i <= units; i ++) {
+            
+            if (i == 0) {
+                System.out.println(i);
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizz buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+            
+        }
+        
+    }
+}
